aaa3d533a82087e6a680e39c55a825dc89e1bc75
J. David Ibáñez <jdavid@itaapy.com>
2011-08-13 22:05:43 +0200

    Drop support for Python 2.5
    It still compiles and probably works fine. But unit tests do not run and
    official support for Python 2.5 has been dropped.

--- test/utils.py
+++ test/utils.py
@@ -29,7 +29,6 @@ __author__ = 'dborowitz@google.com (Dave Borowitz)'
 
 import os
 import shutil
-import sys
 import tarfile
 import tempfile
 import unittest
@@ -45,10 +44,7 @@ class BaseTestCase(unittest.TestCase):
     def assertRaisesWithArg(self, exc_class, arg, func, *args, **kwargs):
         try:
             func(*args, **kwargs)
-        except exc_class:
-            # XXX Use the 'exc_class as exc_value' syntax as soon as we drop
-            # support for Python 2.5
-            exc_value = sys.exc_info()[1]
+        except exc_class as exc_value:
             self.assertEqual((arg,), exc_value.args)
         else:
             self.fail('%s(%r) not raised' % (exc_class.__name__, arg))
