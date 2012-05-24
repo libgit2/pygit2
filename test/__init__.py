2a49eadc57285e6f7d7577c729e41ac987df3128
Nico von Geyso <Nico.Geyso@FU-Berlin.de>
2012-05-24 19:10:00 +0200

    added some tests for diff

--- test/__init__.py
+++ test/__init__.py
@@ -36,7 +36,7 @@ import unittest
 
 
 names = ['blob', 'commit', 'index', 'refs', 'repository', 'revwalk', 'tag',
-         'tree', 'signature', 'status', 'treebuilder']
+         'tree', 'signature', 'status', 'treebuilder', 'diff']
 def test_suite():
     modules = ['test.test_%s' % n for n in names]
     return unittest.defaultTestLoader.loadTestsFromNames(modules)
