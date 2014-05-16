19be4b6aa4a8e0b06f9f7bbe0cfe071ee40e35f5
Carlos Martín Nieto <cmn@dwim.me>
2014-05-16 03:18:14 +0200

    clone: wrap clone_into()
    This allows the user to prepare the repository and remote with whichever
    custom settings they want before performing the "clone" proper.

--- test/test_repository.py
+++ test/test_repository.py
@@ -40,7 +40,7 @@ from os.path import join, realpath
 
 # Import from pygit2
 from pygit2 import GIT_OBJ_ANY, GIT_OBJ_BLOB, GIT_OBJ_COMMIT
-from pygit2 import init_repository, clone_repository, discover_repository
+from pygit2 import init_repository, clone_repository, clone_into, discover_repository
 from pygit2 import Oid, Reference, hashfile
 import pygit2
 from . import utils
@@ -460,6 +460,13 @@ class CloneRepositoryTest(utils.NoRepoTestCase):
             repo_path, self._temp_dir, remote_name="custom_remote")
         self.assertFalse(repo.is_empty)
         self.assertEqual(repo.remotes[0].name, "custom_remote")
+
+    def test_clone_into(self):
+        repo_path = "./test/data/testrepo.git/"
+        repo = init_repository(os.path.join(self._temp_dir, "clone-into"))
+        remote = repo.create_remote("origin", 'file://' + os.path.realpath(repo_path))
+        clone_into(repo, remote)
+        self.assertTrue('refs/remotes/origin/master' in repo.listall_references())
 
     def test_clone_with_credentials(self):
         credentials = pygit2.UserPass("libgit2", "libgit2")
