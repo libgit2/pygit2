9afd7b735b37f8274c9492977bd685f2d312c9ab
Dan Sully <dsully@linkedin.com>
2019-09-06 04:14:39 -0700

    Add comment regarding ffi decl issue.

--- pygit2/decl/oid.h
+++ pygit2/decl/oid.h
@@ -2,6 +2,7 @@ typedef struct git_oid {
 	unsigned char id[20];
 } git_oid;
 
+// This should go to net.h but due to h_order in _run.py, ffi won't compile properly.
 typedef struct git_remote_head {
         int local;
         git_oid oid;
