66673cd6dd9c9c0458802af58bba8f76d1cb0b19
Lukas Berk <lberk@redhat.com>
2019-11-27 20:10:58 -0500

    Fix build, change git_attr_t to git_attr_value_t
    When using libgit2 built from source, the build breaks due to a change
    from git_attr_t to git_attr_value_t.  Update our headerfile
    accordingly.

--- pygit2/decl/attr.h
+++ pygit2/decl/attr.h
@@ -8,7 +8,7 @@ typedef enum {
 	GIT_ATTR_TRUE_T,
 	GIT_ATTR_FALSE_T,
 	GIT_ATTR_VALUE_T,
-} git_attr_t;
+} git_attr_value_t;
 
 int git_attr_get(
 	const char **value_out,
@@ -17,4 +17,4 @@ int git_attr_get(
 	const char *path,
 	const char *name);
 
-git_attr_t git_attr_value(const char *attr);
+git_attr_value_t git_attr_value(const char *attr);
