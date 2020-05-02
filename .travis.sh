a2e65f4ec9a3f63dd108518e4c68e9bac0026dca
J. David Ibáñez <jdavid.ibp@gmail.com>
2020-05-02 12:40:23 +0200

    Travis: build libssh2 1.9.0

--- .travis.sh
+++ .travis.sh
@@ -7,13 +7,23 @@ then
     exit 1
 fi
 
-cd ~
+PREFIX=/home/travis/install
 
+# Build libssh2 1.9.0 (Ubuntu only has 1.8.0, which doesn't work)
+cd ~
+wget https://www.libssh2.org/download/libssh2-1.9.0.tar.gz
+tar xf libssh2-1.9.0.tar.gz
+cd libssh2-1.9.0
+./configure --prefix=/usr --disable-static && make
+sudo make install
+
+# Build libgit2
+cd ~
 git clone --depth=1 -b "maint/v${LIBGIT2_VERSION}" https://github.com/libgit2/libgit2.git
 cd libgit2/
 
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DBUILD_CLAR=OFF
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_CLAR=OFF
 cmake --build . --target install
 
 ls -la ..
