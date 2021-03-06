8ce2c901b6246d479b3ac88d7c705e8c37adae0a
Michał Górny <mgorny@gentoo.org>
2021-03-06 18:06:25 +0100

    Use built-in functools.cached_property in py3.8+
    Python 3.8+ supplies the cached-property functionality in functools
    module, so use that instead of pulling in the backport package whenever
    possible.

--- pygit2/config.py
+++ pygit2/config.py
@@ -23,7 +23,10 @@
 # the Free Software Foundation, 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301, USA.
 
-from cached_property import cached_property
+try:
+    from functools import cached_property
+except ImportError:
+    from cached_property import cached_property
 
 # Import from pygit2
 from .errors import check_error
