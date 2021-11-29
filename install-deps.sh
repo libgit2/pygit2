1a9c401374b1531f7bd39125eb8b385774c51555
Simon Cozens <simon@simon-cozens.org>
2021-11-29 11:01:50 +0000

    Unify the two dependency scripts, for starters

--- install-deps.sh
+++ install-deps.sh
@@ -80,17 +80,61 @@ if [ -n "$ZLIB_VERSION" ]; then
     cd ..
 fi
 
+# Install openssl
+if [ -n "$OPENSSL_VERSION" ]; then
+    FILENAME=openssl-$OPENSSL_VERSION
+    wget https://www.openssl.org/source/$FILENAME.tar.gz -N --no-check-certificate
+
+    tar xf $FILENAME.tar.gz
+    mv $FILENAME openssl-x86
+
+    tar xf $FILENAME.tar.gz
+    mv $FILENAME openssl-arm
+
+    cd openssl-x86
+    ./Configure darwin64-x86_64-cc shared
+    make
+    cd ../openssl-arm
+    ./Configure enable-rc5 zlib darwin64-arm64-cc no-asm
+    make
+    cd ..
+
+    mkdir openssl-universal
+
+    LIBSSL=$(basename openssl-x86/libssl.*.dylib)
+    lipo -create openssl-x86/libssl.*.dylib openssl-arm/libssl.*.dylib -output openssl-universal/$LIBSSL
+    LIBCRYPTO=$(basename openssl-x86/libcrypto.*.dylib)
+    lipo -create openssl-x86/libcrypto.*.dylib openssl-arm/libcrypto.*.dylib -output openssl-universal/$LIBCRYPTO
+    cd openssl-universal
+    install_name_tool -id "@rpath/$LIBSSL" $LIBSSL
+    install_name_tool -id "@rpath/$LIBCRYPTO" $LIBCRYPTO
+    OPENSSL_PREFIX=$(pwd)
+    cd ..
+fi
+
 # Install libssh2
 if [ -n "$LIBSSH2_VERSION" ]; then
     FILENAME=libssh2-$LIBSSH2_VERSION
     wget https://www.libssh2.org/download/$FILENAME.tar.gz -N --no-check-certificate
     tar xf $FILENAME.tar.gz
     cd $FILENAME
-    cmake . \
-            -DCMAKE_INSTALL_PREFIX=$PREFIX \
-            -DBUILD_SHARED_LIBS=ON \
-            -DBUILD_EXAMPLES=OFF \
-            -DBUILD_TESTING=OFF
+    if [ "$KERNEL" = "Darwin" ]; then
+        cmake . \
+                -DCMAKE_INSTALL_PREFIX=$PREFIX \
+                -DBUILD_SHARED_LIBS=ON \
+                -DBUILD_EXAMPLES=OFF \
+                -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+                -DOPENSSL_CRYPTO_LIBRARY="../openssl-universal/$LIBCRYPTO" \
+                -DOPENSSL_SSL_LIBRARY="../openssl-universal/$LIBSSL" \
+                -DOPENSSL_INCLUDE_DIR="../openssl-x86/include" \
+                -DBUILD_TESTING=OFF
+    else
+        cmake . \
+                -DCMAKE_INSTALL_PREFIX=$PREFIX \
+                -DBUILD_SHARED_LIBS=ON \
+                -DBUILD_EXAMPLES=OFF \
+                -DBUILD_TESTING=OFF
+    fi
     cmake --build . --target install
     cd ..
     LIBSSH2_PREFIX=$PREFIX
@@ -102,11 +146,32 @@ if [ -n "$LIBGIT2_VERSION" ]; then
     wget https://github.com/libgit2/libgit2/archive/refs/tags/v$LIBGIT2_VERSION.tar.gz -N -O $FILENAME.tar.gz
     tar xf $FILENAME.tar.gz
     cd $FILENAME
-    CMAKE_PREFIX_PATH=$OPENSSL_PREFIX:$LIBSSH2_PREFIX cmake . \
-            -DBUILD_SHARED_LIBS=ON \
-            -DBUILD_CLAR=OFF \
-            -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
-            -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+    if [ "$KERNEL" = "Darwin" ]; then
+        CMAKE_PREFIX_PATH=$OPENSSL_PREFIX:$LIBSSH2_PREFIX cmake . \
+                -DBUILD_SHARED_LIBS=ON \
+                -DBUILD_CLAR=OFF \
+                -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+                -DOPENSSL_CRYPTO_LIBRARY="../openssl-universal/$LIBCRYPTO" \
+                -DOPENSSL_SSL_LIBRARY="../openssl-universal/$LIBSSL" \
+                -DOPENSSL_INCLUDE_DIR="../openssl-x86/include" \
+                -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+    else
+        CMAKE_PREFIX_PATH=$OPENSSL_PREFIX:$LIBSSH2_PREFIX cmake . \
+                -DBUILD_SHARED_LIBS=ON \
+                -DBUILD_CLAR=OFF \
+                -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+    fi
     cmake --build . --target install
     cd ..
+fi
+
+if [ "$KERNEL" = "Darwin" ]; then
+    if [ "$GITHUB_ACTIONS" != "true" ]; then
+        echo "Refusing to install Universal openssl locally!"
+        exit 1
+    fi
+    # This is gross
+    cp -r $OPENSSL_PREFIX/*.dylib /usr/local/lib
+    cp -r $LIBSSH2_PREFIX/lib/*.dylib /usr/local/lib
+    cp -r $FILENAME/*.dylib /usr/local/lib
 fi
