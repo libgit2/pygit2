aed5916b3cde5c412ddd2de19fc2aae9d63e203a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-03 15:16:00 +0000

    Adding tarfile member sanitization to extractall()

--- test/utils.py
+++ test/utils.py
@@ -114,7 +114,29 @@ class TemporaryRepository:
                 zipf.extractall(self.tmp_path)
         elif path.suffix == '.tar':
             with tarfile.open(path) as tar:
-                tar.extractall(self.tmp_path)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, self.tmp_path)
         elif path.suffix == '.git':
             shutil.copytree(path, temp_repo_path)
         else:
