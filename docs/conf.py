8b92361544e5a718ee9cd1f6364dc42848b38ff1
J. David Ibáñez <jdavid.ibp@gmail.com>
2023-09-24 10:55:18 +0200

    Release 1.13.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2010-2023 The pygit2 contributors'
 #author = ''
 
 # The full version, including alpha/beta/rc tags
-release = '1.13.0'
+release = '1.13.1'
 
 
 # -- General configuration ---------------------------------------------------
