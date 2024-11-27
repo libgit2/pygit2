5c11ece134e667718269b53ef7ac379db68c5773
William Schueller <william.schueller@gmail.com>
2024-11-27 15:20:01 +0100

    test using pygit2 for branch creation instead of subprocess call, but fails because of surrogates not allowed

--- test/test_nonunicode.py
+++ test/test_nonunicode.py
@@ -41,8 +41,7 @@ def bstring(request):
 
 
 def test_nonunicode_branchname(testrepo, bstring):
-    cmd = b"git checkout -b " + bstring
-    subprocess.check_output(cmd.split(b" "), cwd=testrepo.workdir)
+    testrepo.branches.local.create(bstring.decode("utf8", errors="surrogateescape"),commit=testrepo.head.target)
     newrepo = pygit2.clone_repository(
         testrepo.workdir,
         os.path.join(os.path.dirname(testrepo.workdir), "test_nonunicode_repo"),
